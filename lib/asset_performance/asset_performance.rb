30925ab963896fbeab9d3b10d1b3606f451f4f22
William Poindexter <wpoindexter@bimfizzle.com>
2014-01-05 00:20:46 -0500

    get the last created har file for parsing and html file creation

--- lib/asset_performance/asset_performance.rb
+++ lib/asset_performance/asset_performance.rb
@@ -1,8 +1,18 @@
+# to use this formatter, run: cucumber --format AssetPerformance::HtmlFormatter --out features/out/out.html
+
+require 'haml'
+require 'har'
+require_relative 'models/page_asset'
+require_relative 'models/page'
+require_relative 'models/request'
+require_relative 'models/response'
+require_relative 'models/scenario'
+require_relative 'models/timings'
+
 module AssetPerformance
   module AddsAssetPerformance
 
     ENV_OUT_DIR = ENV['ASSET_PERFORMANCE_DIR']
-    DEFAULT_OUT_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..', 'features', 'out'), __FILE__)
 
     def self.formatter_with_asset_performance(formatter_class)
       Class.new(formatter_class) { include AddsAssetPerformance }
@@ -14,29 +24,56 @@ module AssetPerformance
     end
 
     def append_asset_performance_to(feature_element)
-      binding.pry
-      # TODO
+      raise 'Please set the environment variable ASSET_PERFORMANCE_DIR in order to allow the formatter to find your .HAR files.' if ENV_OUT_DIR.nil?\
 
-      #out_dir = ENV_OUT_DIR.nil? ? DEFAULT_OUT_DIR : ENV_OUT_DIR
-      #FileUtils.mkdir_p out_dir unless Dir.exists? out_dir
-      #scenario_out_dir = "#{out_dir}/#{name.downcase.tr(' ', '_')}"
-      #FileUtils.mkdir_p scenario_out_dir unless Dir.exists? scenario_out_dir
+      last_har_file_name = Dir.glob("#{ENV_OUT_DIR}/*.har").last # hack to get the file that was created last
+      har = HAR::Archive.from_file last_har_file_name
 
-      # look for .HAR out directory via environment variable
+      scenario = Scenario.new
+      scenario.title = feature_element.title
 
-      # look for .HAR out directory with default path
+      pages = []
+      har.pages.each do |har_page|
+        page = Page.new
+        page.total_load_time = har_page.timings.on_load
+        page.title = har_page.title
+        har_page.entries.each do |har_asset|
+          asset = PageAsset.new
+          har_request = har_asset.request
+          request = Request.new
+          request.url = har_request.url
+          request.type = har_request.method
+          asset.request = request
+          har_response = har_asset.response
+          response = Response.new
+          response.status = har_response.status
+          response.header_size = har_response.headers_size
+          response.body_size = har_response.body_size
+          response.mime_type = har_response.content.mime_type
+          asset.response = response
+          har_timings = har_asset.timings
+          timings = Timings.new
+          timings.blocked = har_timings.blocked
+          timings.dns = har_timings.dns
+          timings.connect = har_timings.connect
+          timings.send = har_timings.send
+          timings.wait = har_timings.wait
+          timings.receive = har_timings.receive
+          response.timings = timings
+          page.page_assets << asset
+        end
+        pages << page
+      end
 
-      # parse the timestamp on the har file
+      scenario.pages = pages
 
-      # in the scenario's directory, create a directory with the date (%D) of the parsed har file's timestamp
-
-      # in the scenario's date directory, place the har file
-
-      # parse the current har file and add interesting data to the scenario name section of the HTML report
-
-      # TODO waaaay in the future
-
-      # based on a flag, show a histogram of asset performance (all the har files in the out directory)
+      html = Haml::Engine.new(File.read(File.join(File.dirname(__FILE__), 'views', 'asset-performance.haml'))).render(
+          Object.new,
+          dir: File.dirname(__FILE__),
+          start_time: har.pages.first.started_date_time,
+          scenario: scenario
+      )
+      File.open("#{File.join(ENV_OUT_DIR, 'asset-performance.html')}", 'w') { |file| file.write html }
     end
 
   end
