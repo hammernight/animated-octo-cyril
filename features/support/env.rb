362d32f80b23c29d9e9f24d5515143f9b6c18001
William Poindexter <wpoindexter@bimfizzle.com>
2014-01-08 15:01:30 -0500

    using inflectors; default directories

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,10 @@
 require 'cucumber'
 require 'watir-webdriver'
 require 'selenium-webdriver'
+require 'active_support/inflector'
 require 'pry'
 
+I18n.enforce_available_locales = false
+
 DEFAULT_OUT_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..', 'out'), __FILE__)
+DEFAULT_ASSET_PERFORMANCE_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..', 'out'), __FILE__)
